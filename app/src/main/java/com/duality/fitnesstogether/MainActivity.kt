866bdba2d57ed62af0ffec38ebc1cad93b17cf99
Gleb Ryzhov <glebka.r@yandex.ru>
2025-08-08 22:13:30 +0300

    GreetingMenuScreen.kt done

--- app/src/main/java/com/duality/fitnesstogether/MainActivity.kt
+++ app/src/main/java/com/duality/fitnesstogether/MainActivity.kt
@@ -4,17 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import kotlinx.serialization.Serializable
+import com.duality.fitnesstogether.ui.AppNavHost
 
 
 class MainActivity : ComponentActivity() {
@@ -24,32 +14,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppNavHost()
         }
-    }
-}
-
-@Serializable
-object Menu
-
-@Composable
-fun AppNavHost(
-    navController: NavHostController = rememberNavController()
-) {
-    NavHost(
-        navController = navController,
-        startDestination = Menu
-    ) {
-        composable<Menu> {
-            MenuScreen()
-        }
-    }
-}
-
-@Composable
-fun MenuScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "Hello!")
     }
 }
